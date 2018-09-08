689184cdfe622851a105cf21f382672c564af1d0
TatevikJ <tatevik_jalatyan@edu.aua.am>
2018-09-08 16:12:30 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,16 @@
+<script>
+const a=1
+const b=4
+const c=5
+const d=9+"Joe"
+
+if (a===b){
+console.log('yay');
+}else if(a+b===c){
+concole.log('good');
+}
+if(c===d){
+console.log('ninja');
+}else{console.log('ninjaaa')
+}
+</script>
